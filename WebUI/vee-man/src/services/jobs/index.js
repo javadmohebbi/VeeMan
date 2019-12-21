f52ca7702ea0aa5b23089a11c5ebd57d68e55a8b
M. Javad Mohebi <me@mjmohebbi.com>
2019-12-21 18:03:15 +0330

    Job Backup Session

--- WebUI/vee-man/src/services/jobs/index.js
+++ WebUI/vee-man/src/services/jobs/index.js
@@ -1,7 +1,7 @@
 import i18n from '../../i18n'
 import { WebUiConf } from '../../configs'
 import { isAuthenticated } from '../auth'
-
+import _ from 'lodash'
 
 export const GetJobsRelatedToBackupServerListFromServer = (backupServerUID) => {
   const t = i18n.getFixedT()
@@ -36,7 +36,6 @@ export const GetJobsRelatedToBackupServerListFromServer = (backupServerUID) => {
   })
 }
 
-
 export const GetJobBackupSession = (jobId) => {
   const t = i18n.getFixedT()
 
@@ -60,6 +59,41 @@ export const GetJobBackupSession = (jobId) => {
           }
       }
       return json.result
+  })
+  .catch(err => {
+      return {
+          message: t("general.err.server.connection"),
+          error: true,
+          err
+      }
+  })
+}
+
+export const GetJobAllBackupSessions = (jobId) => {
+  const t = i18n.getFixedT()
+
+  // const apiURL = (`${process.env.REACT_APP_API_BASE}auth/login`)
+  const apiURL = (`${WebUiConf().apiURL}/ui/job/${jobId}/backupSession/all`)
+
+  return fetch(apiURL, {
+      method: "GET",
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": isAuthenticated().token
+      },
+  })
+  .then(response => response.json())
+  .then(json => {
+      if (json.hasOwnProperty('error') && json.error !== false) {
+          return {
+              message: t("general.err.list"),
+              error: true
+          }
+      }
+      // return _.reverse(_.sortBy(json.result, ['CreationTimeUTC']));
+      // console.log(_.orderBy(json.result, ['CreationTimeUTC']));
+      return  _.reverse(_.orderBy(json.result, ['CreationTimeUTC']))
   })
   .catch(err => {
       return {
