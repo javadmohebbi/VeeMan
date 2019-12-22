ca04acb5f5604fc11edaf71dd417e334f68b85ce
M. Javad Mohebi <me@mjmohebbi.com>
2019-12-22 11:43:14 +0330

    Fix Repos Percentage

--- WebUI/vee-man/src/components/Repositories/tableRow.jsx
+++ WebUI/vee-man/src/components/Repositories/tableRow.jsx
@@ -6,6 +6,31 @@ const RepoListRow = (props) => {
 
   const { rows=[] } = props
 
+  const Percentage = ({cap=0.0, fre=0.0}) => {
+    var perc = (fre * 100 / cap).toFixed(2)
+    var label = "secondary"
+
+    if (perc <= 10) {
+      label = "danger"
+    } else if (perc > 10 && perc <= 40) {
+      label = "warning"
+    } else if (perc > 40 && perc <= 70) {
+      label = "info"
+    } else {
+      label = "success"
+    }
+
+    return (
+      <span className={`badge badge-xl badge-${label} mr-2 ml-2`}>
+        %
+        {
+          perc
+        }
+      </span>
+    )
+  }
+
+
   return (
     <>
       {
@@ -15,7 +40,10 @@ const RepoListRow = (props) => {
             <td>{row.Repository.Type}</td>
             <td>{row.Repository.Kind}</td>
             <td>{FormatBytes(row.Repository.Capacity)}</td>
-            <td>{FormatBytes(row.Repository.FreeSpace)}</td>
+            <td>
+              {FormatBytes(row.Repository.FreeSpace)}
+              <Percentage cap={parseFloat(row.Repository.Capacity)} fre={parseFloat(row.Repository.FreeSpace)} />
+            </td>
           </tr>
         ))
       }
